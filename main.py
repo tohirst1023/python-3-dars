6fd3c92bcf704ff66619f0533f005c9004ee5856
Tohir Arapjonov <toxirarapjonov@gmail.com>
2024-08-24 14:09:55 +0500

    sdffdfdsfsf

---
+++ main.py
@@ -0,0 +1,52 @@
+
+
+animal1_type = input("Hayvon turi: ")
+animal1_weight = input("Hayvon vazni: ")
+animal1_color = input("Hayvon rangi: ")
+animal1_age = input("Hayvon yoshi: ")
+animal1_name = input("Hayvon nomi: ")
+
+print(f"Hayvon turi: {animal1_type}")
+print(f"Hayvon vazni: {animal1_weight}")
+print(f"Hayvon rangi: {animal1_color}")
+print(f"Hayvon yoshi: {animal1_age}")
+print(f"Hayvon nomi: {animal1_name}")
+
+
+animal2_type = input("Hayvon turi: ")
+animal2_weight = input("Hayvon vazni: ")
+animal2_color = input("Hayvon rangi: ")
+animal2_age = input("Hayvon yoshi: ")
+animal2_name = input("Hayvon nomi: ")
+
+print(f"Hayvon turi: {animal2_type}")
+print(f"Hayvon vazni: {animal2_weight}")
+print(f"Hayvon rangi: {animal2_color}")
+print(f"Hayvon yoshi: {animal2_age}")
+print(f"Hayvon nomi: {animal2_name}")
+
+
+animal3_type = input("Hayvon turi: ")
+animal3_weight = input("Hayvon vazni: ")
+animal3_color = input("Hayvon rangi: ")
+animal3_age = input("Hayvon yoshi: ")
+animal3_name = input("Hayvon nomi: ")
+
+print(f"Hayvon turi: {animal3_type}")
+print(f"Hayvon vazni: {animal3_weight}")
+print(f"Hayvon rangi: {animal3_color}")
+print(f"Hayvon yoshi: {animal3_age}")
+print(f"Hayvon nomi: {animal3_name}")
+
+
+animal4_type = input("Hayvon turi: ")
+animal4_weight = input("Hayvon vazni: ")
+animal4_color = input("Hayvon rangi: ")
+animal4_age = input("Hayvon yoshi: ")
+animal4_name = input("Hayvon nomi: ")
+
+print(f"Hayvon turi: {animal4_type}")
+print(f"Hayvon vazni: {animal4_weight}")
+print(f"Hayvon rangi: {animal4_color}")
+print(f"Hayvon yoshi: {animal4_age}")
+print(f"Hayvon nomi: {animal4_name}")
